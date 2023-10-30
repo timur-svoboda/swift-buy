89d592911989b2643a059b936dc7447b2f665098
Timur Svoboda <svobodatimur@gmail.com>
2023-10-30 16:59:09 +0200

    Add some product cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import type { Metadata } from "next";
 import CssBaseline from "@mui/material/CssBaseline";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "SwiftBuy",
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,52 @@
-import Button from '@mui/material/Button';
+import {
+  Grid,
+  Container,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Box,
+} from "@mui/material";
 
 export default function Home() {
-  return <Button variant="contained">Hello world</Button>;
+  const products = [1, 2, 3, 4, 5, 6, 7];
+
+  return (
+    <Box pt="32px">
+      <Container>
+        <Grid container spacing={2}>
+          {products.map((product) => (
+            <Grid item key={product} xs={3}>
+              <Card>
+                <CardContent>
+                  <Typography
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    Word of the Day
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    asd
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                  </Typography>
+                  <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
