cfc8b95d5098d9d4916bd446bf1e842c6eb7e156
Timur Svoboda <svobodatimur@gmail.com>
2023-10-31 10:57:19 +0200

    Add loader and adding procuts to cart

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,10 @@ import {
   CardMedia,
   Container,
   Grid,
+  IconButton,
   Toolbar,
   Typography,
+  CircularProgress,
 } from "@mui/material";
 import { DatabaseContext, ProductDocument } from "@swift-buy/database";
 import {
@@ -21,18 +23,22 @@ import {
 } from "@swift-buy/ui-components";
 import Image from "next/image";
 import { useCallback, useContext, useEffect, useMemo, useState } from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState("");
 
   const { database } = useContext(DatabaseContext);
+  const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState<ProductDocument[]>([]);
+  const [productsInCart, setProductsInCart] = useState<ProductDocument[]>([]);
 
   useEffect(() => {
     const fetchProcuts = async () => {
       if (database) {
         const products = await database.collections.products.find().exec();
         setProducts(products);
+        setLoading(false);
       }
     };
 
@@ -103,14 +109,91 @@ export default function Home() {
     [trimmedSearchQuery]
   );
 
+  let main: React.ReactNode;
+  if (loading) {
+    main = (
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  } else if (filtredProducts.length) {
+    main = (
+      <Box pt="32px" pb="32px">
+        <Container>
+          <Grid container spacing={2}>
+            {filtredProducts.map((product) => (
+              <Grid item key={product.id} xs={3}>
+                <Card
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    height: "100%",
+                  }}
+                >
+                  <CardMedia>
+                    <Box position="relative" width="100%">
+                      <Box paddingTop="100%" />
+                      <Image src={product.image} alt="" fill sizes="25vw" />
+                    </Box>
+                  </CardMedia>
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography variant="h5" component="div" gutterBottom>
+                      {...highlightSearchQuery(product.title)}
+                    </Typography>
+                    <Typography variant="body2" gutterBottom>
+                      {...highlightSearchQuery(product.description)}
+                    </Typography>
+                  </CardContent>
+                  <CardActions sx={{ justifyContent: "space-between" }}>
+                    <Typography variant="h5" component="div">
+                      ${product.price}
+                    </Typography>
+                    <Button
+                      variant="contained"
+                      onClick={() => {
+                        setProductsInCart([...productsInCart, product]);
+                      }}
+                    >
+                      Add to Cart
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </Box>
+    );
+  } else {
+    main = (
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h2">Nothing found</Typography>
+      </Box>
+    );
+  }
+
   return (
-    <Box sx={{ minHeight: "100vh", display: 'flex', flexDirection: "column" }}>
+    <Box sx={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
             SwiftBuy
           </Typography>
-          <Search>
+          <Search sx={{ marginRight: 4 }}>
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
@@ -120,59 +203,28 @@ export default function Home() {
               onChange={(event) => setSearchQuery(event.target.value)}
             />
           </Search>
+          <IconButton sx={{ position: "relative", color: "white" }}>
+            <ShoppingCartIcon />
+            {!!productsInCart.length && (
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: 4,
+                  right: 4,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: 8,
+                  height: 8,
+                  backgroundColor: "error.light",
+                  borderRadius: "50%",
+                }}
+              />
+            )}
+          </IconButton>
         </Toolbar>
       </AppBar>
-      {filtredProducts.length ? (
-        <Box pt="32px" pb="32px">
-          <Container>
-            <Grid container spacing={2}>
-              {filtredProducts.map((product) => (
-                <Grid item key={product.id} xs={3}>
-                  <Card
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      height: "100%",
-                    }}
-                  >
-                    <CardMedia>
-                      <Box position="relative" width="100%">
-                        <Box paddingTop="100%" />
-                        <Image src={product.image} alt="" fill sizes="25vw" />
-                      </Box>
-                    </CardMedia>
-                    <CardContent sx={{ flexGrow: 1 }}>
-                      <Typography variant="h5" component="div" gutterBottom>
-                        {...highlightSearchQuery(product.title)}
-                      </Typography>
-                      <Typography variant="body2" gutterBottom>
-                        {...highlightSearchQuery(product.description)}
-                      </Typography>
-                    </CardContent>
-                    <CardActions sx={{ justifyContent: "space-between" }}>
-                      <Typography variant="h5" component="div">
-                        ${product.price}
-                      </Typography>
-                      <Button variant="contained">Buy</Button>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
-          </Container>
-        </Box>
-      ) : (
-        <Box
-          sx={{
-            flexGrow: 1,
-            display: 'flex',
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="h2">Nothing found</Typography>
-        </Box>
-      )}
+      {main}
     </Box>
   );
 }
